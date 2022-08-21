f6ca9835ae35b63ed58a60c387a6b81218202822
rejarevaldy <rejarevaldy30@gmail.com>
2022-08-21 15:57:46 +0800

    Template and component

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,32 @@
+<div class="left-side-bar">
+        <div class="brand-logo">
+            <a href="index.html">
+                <h1 class="text-white "><i class="bi bi-boxes"> </i>POS</h1>
+            </a>
+            <div class="close-sidebar" data-toggle="left-sidebar-close">
+                <i class="ion-close-round"></i>
+            </div>
+        </div>
+        <div class="menu-block customscroll">
+            <div class="sidebar-menu">
+                <ul id="accordion-menu">
+                    <li class="dropdown">
+                        <a href="javascript:;" class="dropdown-toggle">
+                            <span class="micon bi bi-house"></span><span class="mtext">Home</span>
+                        </a>
+                        <ul class="submenu">
+                            <li><a href="index.html">Dashboard style 1</a></li>
+                            <li><a href="index2.html">Dashboard style 2</a></li>
+                            <li><a href="index3.html">Dashboard style 3</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="calendar.html" class="dropdown-toggle no-arrow">
+                            <span class="micon bi bi-calendar4-week"></span><span class="mtext">Calendar</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="mobile-menu-overlay"></div>
